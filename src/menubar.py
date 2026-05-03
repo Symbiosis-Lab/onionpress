24d189e7f0e531868ea567ee7dd21095d6d815a5
Brewster Kahle <brewster@archive.org>
2026-05-03 09:00:16 -0700

    Bump version to 2.4.78

--- src/menubar.py
+++ src/menubar.py
@@ -240,7 +240,7 @@ class OnionPressApp(rumps.App):
         self.icon = self.icon_stopped
 
         # Set version to placeholder (will be updated in background)
-        self.version = "2.4.77"
+        self.version = "2.4.78"
 
         # Set up environment variables (fast - no I/O)
         docker_config_dir = os.path.join(self.app_support, "docker-config")
